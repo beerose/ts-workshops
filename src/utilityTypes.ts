d4e09a7f1d354030b55c372447805f146ed1ea66
Aleksandra Sikora <ola.zxcvbnm@gmail.com>
2020-02-09 11:50:59 +0100

    Change example for partial

--- src/utilityTypes.ts
+++ src/utilityTypes.ts
@@ -1,44 +1,50 @@
-type Item = {
-  name: string;
-  size: number;
-  weight: number;
-  dimensions: [number, number, number];
-};
-
-const update(item: Partial<Item>) => {
-    //
-}
-
-type Partial<T> = { [P in keyof T]?: T[P] }; //builtin
-
-type Person = {
+{
+  type Item = {
     name: string;
+    size: number;
+    weight: number;
+    dimensions: [number, number, number];
+  };
+
+  const updateItem = (item: Item, updated: Partial<Item>) => {
+    return {
+      ...item,
+      ...updated,
+    };
+  };
+  // type Partial<T> = { [P in keyof T]?: T[P] }; builtin
 }
 
-const jon: Readonly<Person> = {
-    name: 'Jon',
-};
+{
+  type Person = {
+    name: string;
+  };
 
-jon.name = 'Not Jon'; // error
-/**
- * Cannot assign to 'name' because 
- * it is a read-only property.ts(2540)
- */
+  const jon: Readonly<Person> = {
+    name: 'Jon',
+  };
+
+  //jon.name = 'Not Jon'; // error
+  /**
+   * Cannot assign to 'name' because
+   * it is a read-only property.ts(2540)
+   */
+}
 
 type User = {
-    name: string;
-    surename: string;
-    city: string;
-    country: string;
-    address: string;
-    zipcode: string;
-}
+  name: string;
+  surename: string;
+  city: string;
+  country: string;
+  address: string;
+  zipcode: string;
+};
 
 type UserDisplayInfo = Pick<User, 'name' | 'surename'>;
-type Pick<T, K extends keyof T> = {[key in K]: T[key]};
+// type Pick<T, K extends keyof T> = {[key in K]: T[key]}; builtin
 
 type UserWithoutZipcode = Omit<User, 'zipcode'>;
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+// type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>; builtin
 
 type P = Parameters<(x: string, y: number) => void>;
 // [string, number]

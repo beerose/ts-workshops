a885d79dfdabda0bf1e46c270a2e6e96fb78a403
Aleksandra Sikora <ola.zxcvbnm@gmail.com>
2020-02-15 13:58:51 +0100

    Many various changes

--- exercises/tscExercise.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Running..."
-
-tsc "src/ex_$1" --strict
-
-echo "Done."
--- src/typeGuards.ts
+++ src/typeGuards.ts
@@ -1,7 +1,7 @@
-const isNumber = (x: any): x is number => {
+const isNumber = (x: unknown): x is number => {
   return typeof x === 'number';
 };
 
-const isUser = (x: any): x is User => {
+const isUser = (x: unknown): x is User => {
   return (x as User).name != undefined && typeof (x as User).name === 'string';
 };

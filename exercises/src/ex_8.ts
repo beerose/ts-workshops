eaa67c821fcb4f85798412ad49d2e631b69f4746
Aleksandra Sikora <ola.zxcvbnm@gmail.com>
2020-02-15 18:53:47 +0100

    Add exercises for ts 3.7 features

---
+++ exercises/src/ex_8.ts
@@ -0,0 +1,36 @@
+/**
+ * Exercise 8: Optional Chaining
+ */
+
+/**
+ * Optional chaining lets us write code where TypeScript can immediately stop running some expressions
+ * if we run into a null or undefined.
+ * There is the new ?. operator for optional property accesses.
+ *
+ * For example foo?.bar() means call bar() of foo is defined. If not, stop and return undefined.
+ *
+ * Task 1: Convert the getGold into one line function: get rid of if statement and use optional chaining.
+ */
+
+type Player = {
+  gameState?: {
+    collectedItems?: {
+      gold?: number;
+    };
+  };
+};
+
+const getGold = (player: Player) => {
+  if (
+    player &&
+    player.gameState &&
+    player.gameState.collectedItems &&
+    player.gameState.collectedItems.gold
+  ) {
+    return player.gameState.collectedItems.gold;
+  } else {
+    return undefined;
+  }
+};
+
+const getGold2 = (player: Player) => /** implement me! */;
---
+++ exercises/src/ex_9.ts
@@ -0,0 +1,22 @@
+/**
+ * Exercise 8: Nullish Coalescing
+ */
+
+/**
+ * From TypeScript 3.7 there's a new operator ?? which you can think of as
+ * a way to “fall back” to a default value when dealing with null or undefined.
+ *
+ * For example, `return values.name ?? placeholder` means:
+ * if values.name is defined return values.name, otherwise return placeholder.
+ *
+ * Task: Convert the following code, so it uses ?? operator.
+ */
+
+const displayName = (user: { name?: string }) => {
+  if (user.name !== undefined && user.name !== null) {
+    return user.name;
+  }
+  return '-';
+};
+
+const displayName2 = (user: { name?: string }) => /** implement me! */
